30fd9ac3d99d2e8f0ddad3537e67471f5086b3d6
Gabriel Tonete <gabtopi77@gmail.com>
2022-02-18 08:35:43 -0300

    commit

--- src/components/Tarefa.js
+++ src/components/Tarefa.js
@@ -10,9 +10,9 @@ export const Tarefa = props => {
 
     const getDataText = (dtConclusao, dtPrevisaoConclusao ) => {
         if(dtConclusao) {
-            return `Concluído em: ${moment(dtConclusao).format('Do MMMM YYYY')}`
+            return `Concluído em: ${moment(dtConclusao).format('Do/MM/YYYY')}`
         }else{
-            return `Previsão de conclusão em: ${moment(dtPrevisaoConclusao).format('Do MMMM YYYY')}`
+            return `Previsão de conclusão em: ${moment(dtPrevisaoConclusao).format('Do/MM/YYYY')}`
         }
     };
     
@@ -20,7 +20,7 @@ export const Tarefa = props => {
         <div className={"container-tarefa " + (dataConclusao == null ? "ativo" : "")}>
             <img
                 src={dataConclusao ? check : elipse}
-                alt={dataConclusao ? "tarefa concluída" : "selecine a tarefa"} />
+                alt={dataConclusao ? "tarefa concluída" : "selecione a tarefa"} />
             <div>
                 <p className={dataConclusao ? "concluido" : ""}>{nome}</p>
                 <span>{getDataText(dataConclusao, dataPrevisaoConclusao)}</span>
