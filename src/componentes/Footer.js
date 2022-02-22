303c054ee4cac407dd5c9828ce2e63ee58b624bb
Gabriel Tonete <gabtopi77@gmail.com>
2022-02-22 18:25:27 -0300

    commit

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import adicionar from '../assets/icones/adicionar.svg';
+import github from '../assets/icones/github-original.svg';
+import info from '../assets/icones/info.svg';
+import { Modal } from 'react-bootstrap';
 
 export const Footer = props => {
-    const {showModal} = props;
+    const { showModal } = props;
+    const [mostraModal, setMostraModal] = useState(false);
+
+
     return (
         <div className="container-footer">
-            <button className={localStorage.getItem('accessToken') ? "buttonHome" : "buttonLogin"} onClick={showModal}><img src={adicionar} alt="Adicionar tarefa" />Adicionar tarefa</button>
-            <span className={localStorage.getItem('accessToken') ? "spanHome" : "spanLogin"}>{new Date().getFullYear()} Fvck Rights. <b>USE DADOS FICTÍCIOS.</b></span>
+            {localStorage.getItem('accessToken')
+                ?
+                <>
+                    <button className="adicionarMobile" onClick={showModal}>
+                        <img src={adicionar} alt="Adicionar tarefa" />Adicionar tarefa
+                    </button>
+                    <div className="footerDesktop">
+                        <a href="https://github.com/gabtonete/frontend-task-reactjs" target="_blank">
+                            <img src={github} alt="Acesse o github do projeto" />
+                        </a>
+                        <a onClick={() => setMostraModal(true)}>
+                            <img src={info} alt="Informações de dados do projeto" />
+                        </a>
+                    </div>
+                    <span className="spanDesktop">github/gabtonete - <b>USE DADOS FICTÍCIOS.</b></span>
+                </>
+                :
+                <>
+                    <div className="footerDeslogado">
+                        <a href="https://github.com/gabtonete/frontend-task-reactjs" target="_blank">
+                            <img src={github} alt="Acesse o github do projeto" />
+                        </a>
+                        <a onClick={() => setMostraModal(true)}>
+                            <img src={info} alt="Informações de dados do projeto" />
+                        </a>
+                    </div>
+                    <span className="spanDeslogado">github/gabtonete - <b>USE DADOS FICTÍCIOS.</b></span>
+                </>
+            }
+            <Modal show={mostraModal} onHide={() => setMostraModal(false)} className="container-modal">
+                <Modal.Body>
+                    <p>Todos os dados inseridos na plataforma serão apagados do banco de dados em breve.
+                        <br />
+                        <br />Para mais informações acesse o repositório do github.
+                    </p>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
