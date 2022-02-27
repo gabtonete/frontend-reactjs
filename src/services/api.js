8f5382ea8e5d25b83ab217430d00ce089c0e6791
Gabriel Tonete <90516868+gabtonete@users.noreply.github.com>
2022-02-27 16:25:22 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://apirestgabrielt.herokuapp.com/api/',
+    baseURL: '',
     //baseURL: 'http://localhost:3001/api/',
     timeout: 30000
 });
